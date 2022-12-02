6aa06fc62929ff2e339874171fa47c77908a10c9
krishna <sahkrishna1126@gmail.com>
2022-12-03 00:16:25 +0530

    code

--- java.js
+++ java.js
@@ -2,3 +2,13 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
   }
+
+  function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('body').style.opacity='0.6';
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('body').style.opacity='1';
+}
